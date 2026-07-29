f90b9e6cab583dafa1ca6d5b0793a44db2efa290
paulDev2911 <ph24311@tutamail.com>
2026-07-29 19:44:55 +0200

    add openbao vm

--- main.tf
+++ main.tf
@@ -8,3 +8,20 @@ module "debian13_template" {
   image_url = "https://cloud.debian.org/images/cloud/trixie/latest/debian-13-generic-amd64.qcow2"
   # image_checksum = "..." # optional: SHA512-Checksum eintragen für Reproduzierbarkeit
 }
+module "openbao" {
+  source = "./modules/vm-instance"
+
+  name        = "openbao"
+  node_name   = "pve01"
+  template_id = module.debian13_template.vm_id
+
+  vlan_id    = 40
+  ip_address = "192.168.40.10/24"
+  gateway    = "192.168.40.1"
+
+  cpu_cores = 2
+  memory_mb = 2048
+  disk_size = 20
+
+  ssh_public_key = var.ssh_public_key
+}
